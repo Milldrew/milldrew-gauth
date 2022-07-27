79b177016a011ce439ad6092006fb3c4ccfc3f4a
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-07-27 17:26:45 -0600

    📅  | Wed Jul 27 17:26:45 MDT 2022

---
+++ dist/google-oath.guard.d.ts
@@ -0,0 +1,4 @@
+declare const GoogleOathGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
+export declare class GoogleOathGuard extends GoogleOathGuard_base {
+}
+export {};
